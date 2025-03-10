8a9f04d9543777664671664c621f15a5330acfbf
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-10 21:36:57 +0200

    Rewrite templates -> create base.html and header.html.  Add employees methods -> add. Change club methods -> save, update.

--- routes/clubs.py
+++ routes/clubs.py
@@ -2,13 +2,14 @@
 
 from flask import render_template, request, redirect
 from app import app, db
-from models.models import Club
+from models.models import Club, Employee
 
 
 @app.route('/add-club')
 def add_club():
     """Function add Club object"""
-    return render_template("add_club.html")
+    employees = Employee.query.all()
+    return render_template("add_club.html", employees=employees)
 
 @app.route('/save-club', methods=['POST'])
 def save_club():
@@ -17,6 +18,10 @@ def save_club():
     location = request.form.get("location")
     club = Club(title=title, location=location)  
     db.session.add(club)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.club_id = club.id
+        db.session.add(employee)
     db.session.commit()
     return redirect('/')
 
@@ -32,7 +37,8 @@ def delete_club(id):
 def edit_club(id):
     """Function edit Club's object at id"""
     club = Club.query.get(id)
-    return render_template("add_club.html", club=club)
+    employees = Employee.query.all()
+    return render_template("add_club.html", club=club, employees=employees)
 
 @app.route("/update-club/<int:id>", methods=["POST"])
 def update_club(id):
@@ -41,5 +47,9 @@ def update_club(id):
     club.title = request.form["title"]
     club.location = request.form["location"]
     db.session.add(club)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.club_id = club.id
+        db.session.add(employee)
     db.session.commit()
     return redirect("/")
