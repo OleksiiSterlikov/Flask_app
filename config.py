846251b5b54a44e258fd37d14e84e7e6d4b5aecd
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-15 18:35:23 +0200

    Add migrations

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 class Config:
     """Configuration class"""
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+    
