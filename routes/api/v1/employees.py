82a08dd870803926665f944c0e234f432fc976f3
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-19 21:10:17 +0200

    Create API function create Employee anf filter Employee

--- routes/api/v1/employees.py
+++ routes/api/v1/employees.py
@@ -1,5 +1,6 @@
-from app import api
+from app import api, db
 from flask_restful import Resource
+from flask import request
 from models.models import Employee as EmployeeModel
 
 
@@ -7,11 +8,31 @@ class Employees(Resource):
     """Class creating API from Employee"""
     def get(self):
         """Function getting employee details from API"""
-        employees = EmployeeModel.query.all()
+        filter = request.args
+        query = EmployeeModel.query
+        if len(filter) < 1:
+            employees = query.all()
+        else:
+            for key in filter.keys():
+                employees = query.filter(getattr(EmployeeModel, key) == filter.get(key))
+
         employee_data = []
         for employee in employees:
             employee_data.append(employee.serialize())
         return employee_data
+
+    def post(self):
+        """Function creating employee from API"""
+        data = request.json
+        employee = EmployeeModel(
+            first_name=data.get("first_name"),
+            last_name=data.get("last_name"),
+            email=data.get("email"),
+            club_id=int(data.get("club_id"))
+        )
+        db.session.add(employee)
+        db.session.commit()
+        return employee.serialize()
 
 
 api.add_resource(Employees, '/api/v1/employees')
